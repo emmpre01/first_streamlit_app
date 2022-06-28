3c626bd2ac738350f194b7bb09dd7c0a36f1863f
emmpre01 <107521815+emmpre01@users.noreply.github.com>
2022-06-28 17:33:42 -0500

    Update streamlit_app.py
    added stop command while troubleshooting
    reorganized Fruityvice code into try-except statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError # Used to control flow changes
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -23,16 +24,26 @@ streamlit.dataframe(fruits_to_show)
 
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like infromation about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like infromation about?', 'Kiwi')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # take the json version and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+    
+    #streamlit.write('The user entered', fruit_choice)
                  
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # the following line simply writes the data to the screen --- streamlit.text(fruityvice_response.json()) 
 
-# take the json version and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+
+#Don't run anything past here while we troubleshoot
+streamlit.stop()
 
 # query fruit data
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
